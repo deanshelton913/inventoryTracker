bbd64845b338a5a82960377b41e5fd47464b9e1d
Dean Shelton <deans@vmc.com>
2013-01-25 13:05:42 -0800

    boosh

--- spec/requests/inventory_items_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "InventoryItems" do
-  describe "GET /inventory_items" do
-    it "works! (now write some real specs)" do
-      get inventory_items_path
-    end
-  end
-end
